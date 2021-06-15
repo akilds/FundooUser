d40fd9e4465272d614ba7dd7e556073a1e31e13c
akilds <akilrainadesigan@gmail.com>
2021-06-15 09:02:31 +0530

    Fundoo User

--- src/main/java/com/userregistration/UserRegistrationApplication.java
+++ src/main/java/com/userregistration/UserRegistrationApplication.java
@@ -2,12 +2,14 @@ package com.userregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
+@EnableEurekaClient
 public class UserRegistrationApplication {
 
 	public static void main(String[] args) {
